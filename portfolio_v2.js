b6a4fcfa298f63356f83c7d790a38768a5da9575
Nevyana Momchilova <nevyana.dm@gmail.com>
2020-11-15 21:24:20 +0200

    JS for the Mobile NavMenu; Scroll To Top Button;

---
+++ portfolio_v2.js
@@ -0,0 +1,34 @@
+// NAVIGATION HAMBURGER MENU
+const hamburgerLines = document.querySelector(".hamburger-lines");
+const navMenu = document.querySelector(".nav-menu");
+const navMenuLinks = document.querySelectorAll(".nav-menu li");
+
+hamburgerLines.addEventListener("click", () => {
+    navMenu.classList.toggle("open");
+    navMenuLinks.forEach(link => {
+        link.classList.toggle("full-opacity");
+    });
+});
+
+navMenuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.toggle("open");
+        navMenuLinks.forEach(link => {
+            link.classList.toggle("full-opacity");
+        });
+    });
+});
+
+// SCROLL TO TOP BUTTON
+window.addEventListener("scroll", function() {
+    let scroll = document.querySelector(".scrollToTop");
+    scroll.classList.toggle("active", window.scrollY > 500);
+});
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
